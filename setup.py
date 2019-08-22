af44c0807f0a5500a8b38fee15ddb4bc30575ed3
Devansh Agarwal <devansh.kv@gmail.com>
2019-08-22 11:26:01 -0400

    Added setup.py
    Signed-off-by: Devansh Agarwal <devansh.kv@gmail.com>

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='your',
+    version='0.1.0',
+    packages=['your'],
+    url='http://github.com/devanshkv/your',
+    author='Devansh Agarwal, Kshitij Aggarwal',
+    install_requires=['numpy', 'h5py', 'scikit-image', 'scipy', 'numba', 'astropy'],
+    author_email='da0017@mix.wvu.edu, ka0064@mix.wvu.edu',
+    description='A unified reader for sigproc filterbank and psrfits data',
+    classifiers=[
+        'Natural Language :: English',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Topic :: Scientific/Engineering :: Astronomy']
+)
