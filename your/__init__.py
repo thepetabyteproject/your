e42e363c7b86895ed28f9c653f2db0cdef0b27d7
Devansh Agarwal <devansh.kv@gmail.com>
2019-08-23 17:14:05 -0400

    Read .sf files as well
    Signed-off-by: Devansh Agarwal <devansh.kv@gmail.com>

--- your/__init__.py
+++ your/__init__.py
@@ -8,19 +8,19 @@ class Your(PsrfitsFile, SigprocFile):
     def __init__(self, file):
         if isinstance(file, str):
             ext = os.path.splitext(file)[1]
-            if ext == '.fits':
-                PsrfitsFile.__init__(self, psrfitslist = [file])                
-            elif ext == '.fil':
+            if ext == ".fits" or ext == ".sf":
+                PsrfitsFile.__init__(self, psrfitslist = [file])
+            elif ext == ".fil":
                 SigprocFile.__init__(self, fp=file)
             else:
-                logger.error('Filetype not supported')
+                raise TypeError('Filetype not supported')
         elif isinstance(file, list):
             for f in file:                
                 try:
                     assert os.path.splitext(f)[1] == '.fits'
                 except AssertionError:
-                    log.exception("Filelist contains files other than .fits")
+                    logger.exception("Filelist contains files other than .fits")
             file.sort()
             PsrfitsFile.__init__(self, psrfitslist=file)
         else:
-            logger.error('Input file is neither a string not a list of string')        
+            raise TypeError('Input file is neither a string not a list of string')
