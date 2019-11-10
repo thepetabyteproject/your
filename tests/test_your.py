1a2896369d4390cdccac23e85679366b229d46d3
Kshitij Aggarwal <ka0064@mix.wvu.edu>
2019-11-10 18:53:56 -0500

    Added data for tests

--- tests/test_your.py
+++ tests/test_your.py
@@ -11,13 +11,7 @@ from your.pysigproc import SigprocFile
 
 @pytest.fixture(scope="session", autouse=True)
 def fil_file(tmpdir_factory):
-    temp_dir_path = tmpdir_factory.mktemp("data")
-    download_path = str(temp_dir_path) + '/askap_frb_180417.tgz'
-    url = 'http://astro.phys.wvu.edu/files/askap_frb_180417.tgz'
-    urlretrieve(url, download_path)
-    frb_tar = tarfile.open(download_path)
-    frb_tar.extractall(path=os.path.dirname(download_path))
-    return temp_dir_path.join('28.fil')
+    return 'data/28.fil'
 
 
 def test_pysigproc_obj(fil_file):
