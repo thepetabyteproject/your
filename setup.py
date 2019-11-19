d124b2e3daa4fc6878f34757ad4f09c3eddd1b8b
Devansh Agarwal <devansh.kv@gmail.com>
2019-11-19 13:56:24 -0500

    Fixing CI issues
    Signed-off-by: Devansh Agarwal <devansh.kv@gmail.com>

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ from setuptools import setup
 
 setup(
     name='your',
-    version='0.4.1',
+    version='0.4.2',
     packages=['your'],
     url='http://github.com/devanshkv/your',
     author='Devansh Agarwal, Kshitij Aggarwal',
     scripts=glob.glob('bin/*'),
     package_dir={'your': 'your'},
-    install_requires=['numpy', 'h5py', 'scikit-image', 'scipy', 'numba', 'astropy', 'Cython'],
+    setup_requires=['numpy'],
+    install_requires=['h5py', 'scikit-image', 'scipy', 'numba', 'astropy', 'Cython'],
     tests_require=['pytest'],
     author_email='da0017@mix.wvu.edu, ka0064@mix.wvu.edu',
     zip_safe=False,
