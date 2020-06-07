630ed18fba56b25c54054fbce58adbafa3188b47
Reshma Thomas <rat0022@gpu02.mushroomkingdom>
2020-06-07 17:59:12 -0400

    Adding an argument to input frequency channel range.

--- bin/your_fits2fil.py
+++ bin/your_fits2fil.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python3
-"""
-Converts files in PSRFITS format
-to Filterbank format and combines
-them into a single file.
+#!/usr/bin/env python
+# coding: utf-8
 
-"""
+# In[1]:
+
 
 import argparse
 import glob
@@ -12,16 +10,32 @@ import logging
 import os
 from datetime import datetime
 
+
+# In[2]:
+
+
 import numpy as np
 import tqdm
+import glob
+
+
+# In[3]:
+
 
 from your import Your
 from your.pysigproc import SigprocFile
 
+
+# In[4]:
+
+
 logger = logging.getLogger(__name__)
 
 
-def make_sigproc_obj(filfile, y):
+# In[5]:
+
+
+def make_sigproc_obj(filfile, y, nchans, chan_freq):
     '''
     Use Your class object of the lower band to make Sigproc
     class object with the relevant parameters
@@ -42,34 +56,37 @@ def make_sigproc_obj(filfile, y):
     fil_obj.telescope_id = 6  # use only GBT for now
     fil_obj.data_type = 0
 
-    fil_obj.nchans = y.your_header.nchans
+    fil_obj.nchans = nchans
     fil_obj.foff = y.your_header.foff
-    fil_obj.fch1 = y.your_header.fch1
+    fil_obj.fch1 = chan_freq[1]
     fil_obj.nbeams = 1
     fil_obj.ibeam = 0
     fil_obj.nbits = y.your_header.nbits
     fil_obj.tsamp = y.your_header.tsamp
     fil_obj.tstart = y.your_header.tstart
     fil_obj.nifs = 1  # Only use Intensity values
-
+    
     from astropy.coordinates import SkyCoord
     loc = SkyCoord(y.your_header.ra_deg, y.your_header.dec_deg, unit='deg')
     ra_hms = loc.ra.hms
     dec_dms = loc.dec.dms
-
+    
     fil_obj.src_raj = float(f'{int(ra_hms[0]):02d}{np.abs(int(ra_hms[1])):02d}{np.abs(ra_hms[2]):07.4f}')
     fil_obj.src_dej = float(f'{int(dec_dms[0]):02d}{np.abs(int(dec_dms[1])):02d}{np.abs(dec_dms[2]):07.4f}')
-
+    
     fil_obj.az_start = -1
     fil_obj.za_start = -1
     return fil_obj
 
 
-def write_fil(data, y, filename=None, outdir=None):
+# In[6]:
+
+
+def write_fil(data, y, nchans =None, chan_freq=None, filename=None, outdir=None):
     '''
     Write Filterbank file given the Your object
     :param y: Your object for the PSRFITS files
-    :param data: Data from the PSRFITS files
+    :param nonzerodata: Non-Zero Data from the PSRFITS files
     :param filename: Output name of the Filterbank file
     :param outdir: Output directory for the Filterbank file
     '''
@@ -91,20 +108,23 @@ def write_fil(data, y, filename=None, outdir=None):
             SigprocFile.append_spectra(data, filfile)
 
         else:
-            fil_obj = make_sigproc_obj(filfile, y)
+            fil_obj = make_sigproc_obj(filfile, y,nchans,chan_freq)
             fil_obj.write_header(filfile)
             logger.info(f'Writing {data.shape[0]} spectra to file: {filfile}')
             fil_obj.append_spectra(data, filfile)
 
     except FileNotFoundError:
-        fil_obj = make_sigproc_obj(filfile, y)
+        fil_obj = make_sigproc_obj(filfile, y, nchans,chan_freq)
         fil_obj.write_header(filfile)
         logger.info(f'Writing {data.shape[0]} spectra to file: {filfile}')
         fil_obj.append_spectra(data, filfile)
     logger.info(f'Successfully written data to Filterbank file: {filfile}')
 
 
-def convert(f, outdir=None, filfile=None):
+# In[7]:
+
+
+def convert(f,c=None, outdir=None, filfile=None):
     '''
     reads data from one or more PSRFITS files
     and writes out a Filterbank File.
@@ -114,6 +134,15 @@ def convert(f, outdir=None, filfile=None):
     '''
     y = Your(f)
     fits_header = vars(y.your_header)
+    if c:
+        min_c = int(np.min(c))
+        max_c = int(np.max(c))
+    else:
+        min_c = 0
+        max_c = len(y.chan_freqs)
+        
+    chan_freq = y.chan_freqs[min_c:max_c]
+    nchans = len(chan_freq)
 
     # Calculate loop of spectra
     interval = 4096 * 24
@@ -130,21 +159,25 @@ def convert(f, outdir=None, filfile=None):
     for nstart, nsamp in tqdm.tqdm(zip(nstarts, nsamps), total=len(nstarts)):
         logger.debug(f'Reading spectra {nstart}-{nstart + nsamp} in file {y.filename}')
         data = y.get_data(nstart, nsamp).astype(y.your_header.dtype)
-        logger.info(f'Writing data from spectra {nstart}-{nstart + nsamp} to filterbank')
-        write_fil(data, y, outdir=outdir, filename=filfile)
-        logger.debug(f'Successfully written data from spectra {nstart}-{nstart + nsamp} to filterbank')
+        data = data[:,min_c:max_c]
+       
+        logger.info(f'Writing data from spectra {nstart}-{nstart + nsamp}in the frequency channel range {min_c}-{max_c} to filterbank')
+        write_fil(data, y, nchans = nchans, chan_freq = chan_freq, outdir=outdir, filename=filfile)
+        logger.debug(f'Successfully written data from spectra {nstart}-{nstart + nsamp} in the frequency channel range {min_c}-{max_c} to filterbank')
 
     logging.debug(f'Read all the necessary spectra')
 
 
+# In[8]:
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description="Convert files from PSRFITS format to a single file in Filterbank format.",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(description="Convert files from PSRFITS format to a single file in Filterbank format.",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-v', '--verbose', help='Be verbose', action='store_true')
     parser.add_argument('-f', '--files',
                         help='Paths of PSRFITS files to be converted to a single file in Filterbank format. Surround with quotes, and either use wildcards or separate with spaces',
                         required=True, type=str)
+    parser.add_argument('-c', '--chans', help='Required channels (eg -c 0 4096)', required=False, type=int, nargs=2, default=None)
     parser.add_argument('-o', '--outdir', type=str, help='Output directory for Filterbank file', default='.',
                         required=False)
     parser.add_argument('-fil', '--fil_name', type=str, help='Output name of the Filterbank file', default=None,
@@ -167,5 +200,13 @@ if __name__ == '__main__':
         files = values.files.split(' ')
     else:
         files = glob.glob(values.files)
+        
 
-    convert(files, values.outdir, values.fil_name)
+    convert(files, values.chans, values.outdir, values.fil_name)
+
+
+# In[ ]:
+
+
+
+
