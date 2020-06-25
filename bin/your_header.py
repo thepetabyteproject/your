970456484198c33ad19edeef0a02414cc8a024f2
josephwkania <60994292+josephwkania@users.noreply.github.com>
2020-06-25 18:39:23 -0400

    added support for multiple files to your_header

--- bin/your_header.py
+++ bin/your_header.py
@@ -3,7 +3,7 @@
 Print your header
 """
 import argparse
-import os.path
+import glob
 from your import Your
 
 def nice_print(dic):
@@ -11,23 +11,27 @@ def nice_print(dic):
         print(f"{key : >27}:\t{item}")
 
 def read_header(f):
-    if os.path.isfile(f):
-        y = Your(f)
-        dic = vars(y.your_header)
-        dic['tsamp'] = y.your_header.tsamp
-        dic['nchans'] = y.your_header.nchans
-        dic['foff'] = y.your_header.foff
-        dic['nspectra'] = y.your_header.nspectra
-        nice_print(dic)
-    else:
-        print(f"File: {f} does not exist!")
+    y = Your(f)
+    dic = vars(y.your_header)
+    dic['tsamp'] = y.your_header.tsamp
+    dic['nchans'] = y.your_header.nchans
+    dic['foff'] = y.your_header.foff
+    dic['nspectra'] = y.your_header.nspectra
+    nice_print(dic)
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description="Read header from fits/fil and print the your header",
+        description="Read header from fits/fil files and print the your header",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-f', '--files',
-                        help='Fits or filterbank file to read header.',
+                        help='Fits or filterbank files to read header.',
                         required=True, type=str)
     values = parser.parse_args()
-    read_header(values.files)
+    
+    if len(values.files.split(' ')) > 1:
+        files = values.files.split(' ')
+    else:
+        files = glob.glob(values.files)
+
+    read_header(files)
