6af4aa68ff1d49caed083241031f81342b9355a0
josephwkania <60994292+josephwkania@users.noreply.github.com>
2020-06-25 16:22:07 -0400

    added your_header.py

---
+++ bin/your_header.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Print your header
+"""
+import argparse
+import os.path
+from your import Your
+
+def nice_print(dic):
+    for key, item in dic.items():
+        print(f"{key : >20}:\t{item}")
+
+def read_header(f):
+    if os.path.isfile(f):
+        y = Your(f)	
+        nice_print(vars(y.your_header))
+    else:
+        print(f"File: {f} does not exist!")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Read header from file and print the your header",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-f', '--files',
+                        help='Paths of PSRFITS files to be converted to a single file in Filterbank format. Surround '
+                             'with quotes, and either use wildcards or separate with spaces',
+                        required=True, type=str)
+    values = parser.parse_args()
+    read_header(values.files)
