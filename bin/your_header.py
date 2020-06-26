48128f56e8d7eee0d5fc14dcb7714e1b0444c91e
Devansh Agarwal <devansh.kv@gmail.com>
2020-06-25 23:06:56 -0400

    Adding codecov with github actions
    Signed-off-by: Devansh Agarwal <devansh.kv@gmail.com>

--- bin/your_header.py
+++ bin/your_header.py
@@ -4,11 +4,14 @@ Print your header
 """
 import argparse
 import glob
+
 from your import Your
+
 
 def nice_print(dic):
     for key, item in dic.items():
         print(f"{key : >27}:\t{item}")
+
 
 def read_header(f):
     y = Your(f)
@@ -18,7 +21,7 @@ def read_header(f):
     dic['foff'] = y.your_header.foff
     dic['nspectra'] = y.your_header.nspectra
     nice_print(dic)
-    
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -28,7 +31,7 @@ if __name__ == '__main__':
                         help='Fits or filterbank files to read header.',
                         required=True, type=str)
     values = parser.parse_args()
-    
+
     if len(values.files.split(' ')) > 1:
         files = values.files.split(' ')
     else:
