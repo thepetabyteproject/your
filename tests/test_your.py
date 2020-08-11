705eeeebaa30c6f1a0213009cd7a6d99ce99b001
Kshitij Aggarwal <ka0064@mix.wvu.edu>
2020-08-11 19:25:05 -0400

    added tests for psrfits and candidate

--- tests/test_your.py
+++ tests/test_your.py
@@ -7,7 +7,11 @@ import pytest
 
 from your.candidate import Candidate
 from your.pysigproc import SigprocFile
+from your.psrfits import PsrfitsFile
 
+import os
+os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'
+_install_dir = os.path.abspath(os.path.dirname(__file__))
 
 @pytest.fixture(scope="session", autouse=True)
 def fil_file(tmpdir_factory):
@@ -25,12 +29,40 @@ def test_pysigproc_obj(fil_file):
     assert fil_obj.nchans == 336
 
 
-def test_get_data(fil_file):
+def test_get_data_fil(fil_file):
     fil_obj = SigprocFile(fil_file)
     data = fil_obj.get_data(0, 10)
     assert np.isclose(np.mean(data), 128, atol=1)
 
+def test_psrfits_obj():
+    fits_file = os.path.join(_install_dir, 'data/28.fits')
+    fits_obj = PsrfitsFile([fits_file])
+    assert fits_obj.nchans == 336
+
+def test_get_data_fits():
+    fits_file = os.path.join(_install_dir, 'data/28.fits')
+    fits_obj = PsrfitsFile([fits_file])
+    data = fits_obj.get_data(0, 10)
+    assert np.isclose(np.mean(data), 128, atol=1)
 
 def test_Candidate(fil_file):
     cand = Candidate(fp=fil_file, dm=475.28400, tcand=2.0288800, width=2, label=-1, snr=16.8128, min_samp=256, device=0)
     assert np.isclose(cand.dispersion_delay(), 0.6254989199749227, atol=1e-3)
+
+def test_candidate_chunk_and_dedispersion():
+    fil = os.path.join(_install_dir, 'data/28.fil')
+    cand = Candidate(fp=fil, dm=475.28400, tcand=2.0288800, width=2, label=-1, snr=16.8128, min_samp=256, device=0)
+    cand.get_chunk()
+    assert np.isclose(np.mean(cand.data), 128, atol=1)
+    cand.dedisperse()
+    assert np.isclose(np.max(cand.dedispersed.T.sum(0)), 47527, atol=1)
+    assert np.isclose(np.max(cand.dedispersets()), 47527, atol=1)
+
+    fnout = cand.save_h5()
+    assert os.path.isfile(fnout)
+    os.remove(fnout)
+
+
+
+
+
