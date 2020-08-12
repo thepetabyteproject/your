b951fe40ac41bf18a8c4054cbb8124291f42b110
Kshitij Aggarwal <ka0064@mix.wvu.edu>
2020-08-12 16:04:28 -0400

    added tests for writer

---
+++ tests/test_writer.py
@@ -0,0 +1,38 @@
+from your.writer import Writer
+from your import Your
+
+import os
+_install_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+def test_to_fil():
+    file = os.path.join(_install_dir, 'data/small.fil')
+    f = Your(file)
+    w = Writer(f)
+    w.to_fil(nstart=0, nsamp=10, filfile='temp.fil')
+    assert os.path.isfile('temp.fil')
+    os.remove('temp.fil')
+
+    file = os.path.join(_install_dir, 'data/small.fits')
+    f = Your(file)
+    w = Writer(f)
+    w.to_fil(nstart=0, nsamp=10, filfile='temp.fil')
+    assert os.path.isfile('temp.fil')
+    os.remove('temp.fil')
+
+
+def test_to_fits():
+    file = os.path.join(_install_dir, 'data/small.fil')
+    f = Your(file)
+    w = Writer(f)
+    w.to_fits(fitsfile='temp.fits')
+    assert os.path.isfile('temp.fits')
+    os.remove('temp.fits')
+
+    file = os.path.join(_install_dir, 'data/small.fits')
+    f = Your(file)
+    w = Writer(f)
+    w.to_fits(fitsfile='temp.fits')
+    assert os.path.isfile('temp.fits')
+    os.remove('temp.fits')
+
