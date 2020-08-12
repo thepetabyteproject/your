ae7faed554fb5178e1ed5330deb32aa1e8f99169
Kshitij Aggarwal <ka0064@mix.wvu.edu>
2020-08-12 16:07:32 -0400

    fixed path of data

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -9,14 +9,14 @@ def test_to_fil():
     file = os.path.join(_install_dir, 'data/small.fil')
     f = Your(file)
     w = Writer(f)
-    w.to_fil(nstart=0, nsamp=10, filfile='temp.fil')
+    w.to_fil(nstart=0, nsamp=10, filfile='temp.fil', outdir='./')
     assert os.path.isfile('temp.fil')
     os.remove('temp.fil')
 
     file = os.path.join(_install_dir, 'data/small.fits')
     f = Your(file)
     w = Writer(f)
-    w.to_fil(nstart=0, nsamp=10, filfile='temp.fil')
+    w.to_fil(nstart=0, nsamp=10, filfile='temp.fil', outdir='./')
     assert os.path.isfile('temp.fil')
     os.remove('temp.fil')
 
@@ -25,14 +25,14 @@ def test_to_fits():
     file = os.path.join(_install_dir, 'data/small.fil')
     f = Your(file)
     w = Writer(f)
-    w.to_fits(fitsfile='temp.fits')
+    w.to_fits(fitsfile='temp.fits', outdir='./')
     assert os.path.isfile('temp.fits')
     os.remove('temp.fits')
 
     file = os.path.join(_install_dir, 'data/small.fits')
     f = Your(file)
     w = Writer(f)
-    w.to_fits(fitsfile='temp.fits')
+    w.to_fits(fitsfile='temp.fits', outdir='./')
     assert os.path.isfile('temp.fits')
     os.remove('temp.fits')
 
