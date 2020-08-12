5cecd694045f23c2756d421520bc32975773930a
Kshitij Aggarwal <ka0064@mix.wvu.edu>
2020-08-11 20:07:59 -0400

    moved pysigproc tests

---
+++ tests/test_pysigproc.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+from your.pysigproc import SigprocFile
+
+import os
+_install_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+def test_pysigproc_obj():
+    fil_file = os.path.join(_install_dir, 'data/28.fil')
+    fil_obj = SigprocFile(fil_file)
+    assert fil_obj.nchans == 336
+
+
+def test_get_data_fil():
+    fil_file = os.path.join(_install_dir, 'data/28.fil')
+    fil_obj = SigprocFile(fil_file)
+    data = fil_obj.get_data(0, 10)
+    assert np.isclose(np.mean(data), 128, atol=1)
