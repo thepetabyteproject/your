86b3b85980f193bf76b56beedadcc9fd721267dd
Devansh Agarwal <devansh.kv@gmail.com>
2020-08-17 18:20:39 -0400

    More coverage!
    Signed-off-by: Devansh Agarwal <devansh.kv@gmail.com>

--- tests/test_candidate.py
+++ tests/test_candidate.py
@@ -1,11 +1,19 @@
 import os
 
 import numpy as np
+import pytest
 
 from your.candidate import Candidate
 
 os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'
 _install_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+@pytest.fixture(scope="function", autouse=True)
+def cand():
+    fil_file = os.path.join(_install_dir, 'data/28.fil')
+    cand = Candidate(fp=fil_file, dm=475.28400, tcand=2.0288800, width=2, label=-1, snr=16.8128, min_samp=256, device=0)
+    return cand
 
 
 def test_Candidate():
@@ -15,16 +23,32 @@ def test_Candidate():
     assert np.isclose(cand.dispersion_delay(), 0.6254989199749227, atol=1e-3)
 
 
-def test_candidate_chunk_and_dedispersion():
-    fil_file = os.path.join(_install_dir, 'data/28.fil')
-    cand = Candidate(fp=fil_file, dm=475.28400, tcand=2.0288800, width=2, label=-1, snr=16.8128, min_samp=256, device=0)
+def test_candidate_chunk(cand):
     cand.get_chunk()
     assert np.isclose(np.mean(cand.data), 128, atol=1)
+
+
+def test_dedispersion_none(cand):
+    cand.dedisperse()
+    assert cand.dedispersed == None
+
+
+def test_dedisperse(cand):
+    cand.get_chunk()
     cand.dedisperse()
     assert np.isclose(np.max(cand.dedispersed.T.sum(0)), 47527, atol=1)
     assert np.isclose(np.max(cand.dedispersets()), 47527, atol=1)
+
+
+def test_dmtime(cand):
+    cand.get_chunk()
+    cand.dedisperse()
     cand.dmtime()
     assert cand.dmt.shape[0] == 256
+
     fnout = cand.save_h5()
     assert os.path.isfile(fnout)
     os.remove(fnout)
+
+    assert pytest.approx(cand.get_snr(), rel=2) == 13
+    assert pytest.approx(cand.optimize_dm()[0], rel=2) == 475
