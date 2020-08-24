63326234bbfe5de565572ce7ffe14c93212c0c93
josephwkania <60994292+josephwkania@users.noreply.github.com>
2020-08-24 13:20:51 -0400

    made time series and bandpass take up full width

--- bin/your_viewer.py
+++ bin/your_viewer.py
@@ -104,7 +104,10 @@ class Paint(Frame):
         self.im_ft = ax2.imshow(self.data, aspect='auto', vmin=self.vmin, vmax=self.vmax) # later use a.set_data(new_data)
         bandpass = np.mean(self.data, axis=1)
         self.im_bandpass, = ax4.plot(bandpass, np.linspace(self.yr.your_header.nchans, 0, len(bandpass)))
-        self.im_time,  = ax1.plot(np.mean(self.data,axis=0))
+        ax4.set_ylim([-1, len(bandpass)+1])
+        time_series = np.mean(self.data,axis=0)
+        self.im_time,  = ax1.plot(time_series)
+        ax1.set_xlim(-1, len(time_series+1))
         #ax.set_xticklabels(np.linspace(0,self.yr.your_header.nchans-1,8))
         
         #plt.colorbar(orientation='vertical', pad=0.01, aspect=30)
