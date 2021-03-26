e80574785e91b9624293d47bb367a2b370df3929
Devansh Agarwal <devansh.kv@gmail.com>
2021-03-26 14:25:13 -0400

    Support for python 3.9
    Signed-off-by: Devansh Agarwal <devansh.kv@gmail.com>

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Topic :: Scientific/Engineering :: Astronomy",
     ],
